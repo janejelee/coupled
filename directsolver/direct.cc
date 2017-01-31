e2ec94f53cd48911e52dee1c2150910fe1f35897
Jane Lee <jane.lee@maths.ox.ac.uk>
2017-01-31 10:17:00 +0000

    preconditioner stuff deleted

--- directsolver/direct.cc
+++ directsolver/direct.cc
@@ -75,24 +75,9 @@ namespace Step22
         const double p_bottom = 1.0;
         const int dimension = 2;
         
-    };
+    }
     
-    
-    template <int dim>
-    struct InnerPreconditioner;
-    
-    template <>
-    struct InnerPreconditioner<2>
-    {
-        typedef SparseDirectUMFPACK type;
-    };
-    
-    template <>
-    struct InnerPreconditioner<3>
-    {
-        typedef SparseILU<double> type;
-    };
-    
+
     
     
     template <int dim>
@@ -123,99 +108,13 @@ namespace Step22
         BlockVector<double> solution;
         BlockVector<double> system_rhs;
         
-        std_cxx11::shared_ptr<typename InnerPreconditioner<dim>::type> A_preconditioner;
+
     };
     
   ////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////
     
 
-
-    
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    
-
-    template <class Matrix, class Preconditioner>
-    class InverseMatrix : public Subscriptor
-    {
-    public:
-        InverseMatrix (const Matrix         &m,
-                       const Preconditioner &preconditioner);
-        
-        void vmult (Vector<double>       &dst,
-                    const Vector<double> &src) const;
-        
-    private:
-        const SmartPointer<const Matrix> matrix;
-        const SmartPointer<const Preconditioner> preconditioner;
-    };
-    
-    
-    template <class Matrix, class Preconditioner>
-    InverseMatrix<Matrix,Preconditioner>::InverseMatrix (const Matrix &m,
-                                                         const Preconditioner &preconditioner)
-    :
-    matrix (&m),
-    preconditioner (&preconditioner)
-    {}
-    
-    
-    
-    template <class Matrix, class Preconditioner>
-    void InverseMatrix<Matrix,Preconditioner>::vmult (Vector<double>       &dst,
-                                                      const Vector<double> &src) const
-    {
-        SolverControl solver_control (src.size(), 1e-6*src.l2_norm());
-        SolverCG<>    cg (solver_control);
-        
-        dst = 0;
-        
-        cg.solve (*matrix, dst, src, *preconditioner);
-    }
-    
-    
-    
-    template <class Preconditioner>
-    class SchurComplement : public Subscriptor
-    {
-    public:
-        SchurComplement (const BlockSparseMatrix<double> &system_matrix,
-                         const InverseMatrix<SparseMatrix<double>, Preconditioner> &A_inverse);
-        
-        void vmult (Vector<double>       &dst,
-                    const Vector<double> &src) const;
-        
-    private:
-        const SmartPointer<const BlockSparseMatrix<double> > system_matrix;
-        const SmartPointer<const InverseMatrix<SparseMatrix<double>, Preconditioner> > A_inverse;
-        
-        mutable Vector<double> tmp1, tmp2;
-    };
-    
-    
-    
-    template <class Preconditioner>
-    SchurComplement<Preconditioner>::
-    SchurComplement (const BlockSparseMatrix<double> &system_matrix,
-                     const InverseMatrix<SparseMatrix<double>,Preconditioner> &A_inverse)
-    :
-    system_matrix (&system_matrix),
-    A_inverse (&A_inverse),
-    tmp1 (system_matrix.block(0,0).m()),
-    tmp2 (system_matrix.block(0,0).m())
-    {}
-    
-    
-    template <class Preconditioner>
-    void SchurComplement<Preconditioner>::vmult (Vector<double>       &dst,
-                                                 const Vector<double> &src) const
-    {
-        system_matrix->block(0,1).vmult (tmp1, src);
-        A_inverse->vmult (tmp2, tmp1);
-        system_matrix->block(1,0).vmult (dst, tmp2);
-    }
-    
     
     ///////////////////////////////////////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -236,7 +135,7 @@ namespace Step22
     template <int dim>
     void StokesProblem<dim>::setup_dofs ()
     {
-        A_preconditioner.reset ();
+
         system_matrix.clear ();
         
         dof_handler.distribute_dofs (fe);
@@ -462,9 +361,11 @@ namespace Step22
     template <int dim>
     void StokesProblem<dim>::solve ()
     {
+
         SparseDirectUMFPACK  A_direct;
         A_direct.initialize(system_matrix);
         A_direct.vmult (solution, system_rhs);
+
     }
 
     
@@ -555,7 +456,7 @@ namespace Step22
 
         
         
-        triangulation.refine_global (3);
+        triangulation.refine_global (4);
         
         for (unsigned int refinement_cycle = 0; refinement_cycle<2;
              ++refinement_cycle)
