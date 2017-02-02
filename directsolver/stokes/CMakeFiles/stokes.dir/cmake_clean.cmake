785b32ad38ebde8fb3a6062b57baab105cbd7f0c
Jane Lee <jane.lee@maths.ox.ac.uk>
2017-02-02 17:26:13 +0000

    still needs fixing. pushing for end of day to work on laptop.

---
+++ directsolver/stokes/CMakeFiles/stokes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stokes.dir/stokes.cc.o"
+  "stokes.pdb"
+  "stokes"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stokes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
