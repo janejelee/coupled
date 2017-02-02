a61ae631bc8ced4fe0e78fcc1f9b6e2327b86a81
Jane Lee <jane.lee@maths.ox.ac.uk>
2017-02-02 17:19:45 +0000

    update stokes system equations. need some work. closing for end of day.

---
+++ stokes/CMakeFiles/stokes_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stokes_main.dir/stokes_main.cc.o"
+  "stokes_main.pdb"
+  "stokes_main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stokes_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
