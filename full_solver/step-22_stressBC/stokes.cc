55819747e7c2a5019f6f0fb0a93307f4d0a7af75
Jane Lee <jane.lee@maths.ox.ac.uk>
2018-02-18 17:55:00 +0000

    dirichlet top and bottom and no flux sides working

--- full_solver/step-22_stressBC/stokes.cc
+++ full_solver/step-22_stressBC/stokes.cc
@@ -5,8 +5,6 @@
 #include <deal.II/lac/block_vector.h>
 #include <deal.II/lac/full_matrix.h>
 #include <deal.II/lac/block_sparse_matrix.h>
-#include <deal.II/lac/solver_cg.h>
-#include <deal.II/lac/precondition.h>
 #include <deal.II/lac/constraint_matrix.h>
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/grid_generator.h>
@@ -27,13 +25,38 @@
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/numerics/error_estimator.h>
 #include <deal.II/lac/sparse_direct.h>
-#include <deal.II/lac/sparse_ilu.h>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 namespace Step22
 {
     using namespace dealii;
+    using namespace numbers;
+    
+    namespace data
+    {
+        const double eta = 1.0;
+        const double lambda = 1.0;
+        const double perm_const = 1.0;
+        const double rho_f = 1.0;
+        const double rho_r = 1.0;
+        const double gamma = 1.0;
+        const double pr_constant = 0.0;
+        
+        const double top = 1.0;
+        const double bottom = 0.0;
+        const double left = 0.0;
+        const double right = PI;
+        
+        const int dimension = 2;
+        const int problem_degree = 1;
+        const int refinement_level = 4;
+        
+        const double timestep = 1e-2;
+        const double final_time = 5*timestep;
+        const double error_time = 13;
+    }
+    
     
     template <int dim>
     class StokesProblem
@@ -45,7 +68,7 @@ namespace Step22
         void setup_dofs ();
         void assemble_system ();
         void solve ();
-        void output_results (const unsigned int refinement_cycle) const;
+        void output_results () const;
         void refine_mesh ();
         const unsigned int   degree;
         Triangulation<dim>   triangulation;
@@ -58,62 +81,79 @@ namespace Step22
         BlockVector<double> system_rhs;
         
     };
+    
     template <int dim>
-    class BoundaryValues : public Function<dim>
+    class RockTopStress : public Function<dim>
     {
     public:
-        BoundaryValues () : Function<dim>(dim+1) {}
-        virtual double value (const Point<dim>   &p,
-                              const unsigned int  component = 0) const;
-        virtual void vector_value (const Point<dim> &p,
-                                   Vector<double>   &value) const;
+        RockTopStress () : Function<dim>(dim+1) {}
+        virtual void vector_value (const Point<dim> &p, Vector<double>   &value) const;
     };
-    template <int dim>
-    double
-    BoundaryValues<dim>::value (const Point<dim>  &p,
-                                const unsigned int component) const
-    {
-        Assert (component < this->n_components,
-                ExcIndexRange (component, 0, this->n_components));
-        if (component == 0)
-            return (p[0] < 0 ? -1 : (p[0] > 0 ? 1 : 0));
-        return 0;
-    }
+    
     template <int dim>
     void
-    BoundaryValues<dim>::vector_value (const Point<dim> &p,
-                                       Vector<double>   &values) const
+    RockTopStress<dim>::vector_value (const Point<dim> &p, Vector<double>   &values) const
     {
-        for (unsigned int c=0; c<this->n_components; ++c)
-            values(c) = BoundaryValues<dim>::value (p, c);
-    }
-    template <int dim>
-    class RightHandSide : public Function<dim>
-    {
-    public:
-        RightHandSide () : Function<dim>(dim+1) {}
-        virtual double value (const Point<dim>   &p,
-                              const unsigned int  component = 0) const;
-        virtual void vector_value (const Point<dim> &p,
-                                   Vector<double>   &value) const;
-    };
-    template <int dim>
-    double
-    RightHandSide<dim>::value (const Point<dim>  &/*p*/,
-                               const unsigned int /*component*/) const
-    {
-        return 0;
-    }
-    template <int dim>
-    void
-    RightHandSide<dim>::vector_value (const Point<dim> &p,
-                                      Vector<double>   &values) const
-    {
-        for (unsigned int c=0; c<this->n_components; ++c)
-            values(c) = RightHandSide<dim>::value (p, c);
+        values(0) = 0;
+        values(1) = -(p[1]*p[1]*p[1] + 4*p[1]);
+        values(2) = 0;
     }
     
+    template <int dim>
+    class RockBottomStress : public Function<dim>
+    {
+    public:
+        RockBottomStress () : Function<dim>(dim+1) {}
+        virtual void vector_value (const Point<dim> &p, Vector<double>   &value) const;
+    };
+    
+    template <int dim>
+    void
+    RockBottomStress<dim>::vector_value (const Point<dim> &p, Vector<double>   &values) const
+    {
+        values(0) = 0;
+        values(1) = -(p[1]*p[1]*p[1] + 4*p[1]);
+        values(2) = 0;
+    }
+    
+    
+    template <int dim>
+    class RockExactSolution : public Function<dim>
+    {
+    public:
+        RockExactSolution () : Function<dim>(dim+1) {}
+        virtual void vector_value (const Point<dim> &p,
+                                   Vector<double>   &value) const;
+    };
 
+    template <int dim>
+    void
+    RockExactSolution<dim>::vector_value (const Point<dim> &p,
+                                          Vector<double>   &values) const
+    {
+        values(0) = 0;
+        values(1) = -p[1]*p[1];
+        values(2) = p[1]*p[1]*p[1];
+    }
+    
+    template <int dim>
+    class ExtraRHSRock : public Function<dim>
+    {
+    public:
+        ExtraRHSRock () : Function<dim>(dim+1) {}
+        virtual void vector_value (const Point<dim> &p, Vector<double>   &value) const;
+    };
+    
+    template <int dim>
+    void
+    ExtraRHSRock<dim>::vector_value (const Point<dim> &p, Vector<double>   &values) const
+    {
+
+        values(0) = 0;
+        values(1) = 4.0 + 3*p[1]*p[1];
+        values(2) = 2*p[1];
+    }
+    
 
     template <int dim>
     StokesProblem<dim>::StokesProblem (const unsigned int degree)
@@ -136,16 +176,32 @@ namespace Step22
         DoFRenumbering::component_wise (dof_handler, block_component);
         {
             constraints.clear ();
+            
             FEValuesExtractors::Vector velocities(0);
+            FEValuesExtractors::Scalar pressure (dim);
+            
             DoFTools::make_hanging_node_constraints (dof_handler,
                                                      constraints);
             VectorTools::interpolate_boundary_values (dof_handler,
                                                       1,
-                                                      BoundaryValues<dim>(),
+                                                      RockExactSolution<dim>(),
                                                       constraints,
                                                       fe.component_mask(velocities));
+            VectorTools::interpolate_boundary_values (dof_handler,
+                                                      2,
+                                                      RockExactSolution<dim>(),
+                                                      constraints,
+                                                      fe.component_mask(velocities));
+            
+            std::set<types::boundary_id> no_normal_flux_boundaries;
+            no_normal_flux_boundaries.insert (0);
+            VectorTools::compute_no_normal_flux_constraints (dof_handler, 0,
+                                                             no_normal_flux_boundaries,
+                                                             constraints);
+            
         }
         constraints.close ();
+        
         std::vector<types::global_dof_index> dofs_per_block (2);
         DoFTools::count_dofs_per_block (dof_handler, dofs_per_block, block_component);
         const unsigned int n_u = dofs_per_block[0],
@@ -183,24 +239,41 @@ namespace Step22
         system_matrix=0;
         system_rhs=0;
         QGauss<dim>   quadrature_formula(degree+2);
+        QGauss<dim-1> face_quadrature_formula(degree+2);
+        
         FEValues<dim> fe_values (fe, quadrature_formula,
                                  update_values    |
                                  update_quadrature_points  |
                                  update_JxW_values |
                                  update_gradients);
+        FEFaceValues<dim> fe_face_values ( fe, face_quadrature_formula,
+                                               update_values | update_normal_vectors |
+                                               update_quadrature_points |update_JxW_values   );
+        
         const unsigned int   dofs_per_cell   = fe.dofs_per_cell;
         const unsigned int   n_q_points      = quadrature_formula.size();
+        const unsigned int   n_face_q_points = face_quadrature_formula.size();
+        
         FullMatrix<double>   local_matrix (dofs_per_cell, dofs_per_cell);
         Vector<double>       local_rhs (dofs_per_cell);
         std::vector<types::global_dof_index> local_dof_indices (dofs_per_cell);
-        const RightHandSide<dim>          right_hand_side;
+        
+        const ExtraRHSRock<dim>          right_hand_side;
         std::vector<Vector<double> >      rhs_values (n_q_points,
                                                       Vector<double>(dim+1));
+        
+        const RockTopStress<dim>        topstress;
+        const RockBottomStress<dim>     bottomstress;
+        std::vector<Vector<double> >      topstress_values (n_face_q_points, Vector<double>      (dim+1));
+        std::vector<Vector<double> >      bottomstress_values (n_face_q_points, Vector<double>      (dim+1));
+        
         const FEValuesExtractors::Vector velocities (0);
         const FEValuesExtractors::Scalar pressure (dim);
+        
         std::vector<SymmetricTensor<2,dim> > symgrad_phi_u (dofs_per_cell);
         std::vector<double>                  div_phi_u   (dofs_per_cell);
         std::vector<double>                  phi_p       (dofs_per_cell);
+        
         typename DoFHandler<dim>::active_cell_iterator
         cell = dof_handler.begin_active(),
         endc = dof_handler.end();
@@ -230,21 +303,96 @@ namespace Step22
                     }
                     const unsigned int component_i =
                     fe.system_to_component_index(i).first;
+                    
                     local_rhs(i) += fe_values.shape_value(i,q) *
-                    rhs_values[q](component_i) *
-                    fe_values.JxW(q);
+                                        rhs_values[q](component_i) *
+                                                        fe_values.JxW(q);
                 }
             }
+            
+//            for (unsigned int face_number=0; face_number<GeometryInfo<dim>::faces_per_cell; ++face_number)
+//                if (cell->face(face_number)->at_boundary()
+//                    &&
+//                    (cell->face(face_number)->boundary_id() == 1))
+//                {
+//                    fe_face_values.reinit (cell, face_number);
+//
+//                    topstress.vector_value_list(fe_face_values.get_quadrature_points(),
+//                                                topstress_values);
+//
+//                    for (unsigned int q_point=0; q_point<n_face_q_points; ++q_point)
+//                    {
+//
+//                        for (unsigned int i=0; i<dofs_per_cell; ++i)
+//                        {
+//
+//                            const unsigned int component_i = fe.system_to_component_index(i).first;
+//
+//                            local_rhs(i) += (
+//                                             topstress_values[q_point](component_i)*
+//                                             fe_face_values.
+//                                             shape_value(i,q_point) *
+//                                             fe_face_values.JxW(q_point));
+//                        }
+//                    }
+//                }
+//
+//            for (unsigned int face_number=0; face_number<GeometryInfo<dim>::faces_per_cell; ++face_number)
+//                if (cell->face(face_number)->at_boundary()
+//                    &&
+//                    (cell->face(face_number)->boundary_id() == 2))
+//                {
+//
+//                    fe_face_values.reinit (cell, face_number);
+//
+//                    bottomstress.vector_value_list(fe_face_values.get_quadrature_points(),
+//                                                   bottomstress_values);
+//
+//                    for (unsigned int q_point=0; q_point<n_face_q_points; ++q_point)
+//                    {
+//                        for (unsigned int i=0; i<dofs_per_cell; ++i)
+//                        {
+//                            const unsigned int component_i = fe.system_to_component_index(i).first;
+//                            local_rhs(i) += (bottomstress_values[q_point](component_i)*
+//                                             fe_face_values.
+//                                             shape_value(i,q_point) *
+//                                             fe_face_values.JxW(q_point));
+//                        }
+//                    }
+//                }
             for (unsigned int i=0; i<dofs_per_cell; ++i)
                 for (unsigned int j=i+1; j<dofs_per_cell; ++j)
                     local_matrix(i,j) = local_matrix(j,i);
             cell->get_dof_indices (local_dof_indices);
+            
             constraints.distribute_local_to_global (local_matrix, local_rhs,
                                                     local_dof_indices,
                                                     system_matrix, system_rhs);
         }
-
+        // Need a condition as Dirichlet conditions as is makes the pressure only determined to a constant
+        
+        std::map<types::global_dof_index, double> pr_determination;
+        {
+            types::global_dof_index n_dofs = dof_handler.n_dofs();
+            std::vector<bool> componentVector(dim + 1, false);
+            componentVector[dim] = true;
+            
+            std::vector<bool> selected_dofs(n_dofs);
+            std::set< types::boundary_id > boundary_ids;
+            boundary_ids.insert(2);
+            
+            DoFTools::extract_boundary_dofs(dof_handler, ComponentMask(componentVector),
+                                            selected_dofs, boundary_ids);
+            
+            for (types::global_dof_index i = 0; i < n_dofs; i++)
+            {
+                if (selected_dofs[i]) pr_determination[i] = data::pr_constant;
+            }
+        }
+        MatrixTools::apply_boundary_values(pr_determination,
+                                           system_matrix, solution, system_rhs);
     }
+    
     template <int dim>
     void StokesProblem<dim>::solve ()
     {
@@ -256,7 +404,7 @@ namespace Step22
     
     template <int dim>
     void
-    StokesProblem<dim>::output_results (const unsigned int refinement_cycle)  const
+    StokesProblem<dim>::output_results ()  const
     {
         std::vector<std::string> solution_names (dim, "velocity");
         solution_names.push_back ("pressure");
@@ -272,12 +420,12 @@ namespace Step22
                                   data_component_interpretation);
         data_out.build_patches ();
         std::ostringstream filename;
-        filename << "solution-"
-        << Utilities::int_to_string (refinement_cycle, 2)
+        filename << "solution"
         << ".vtk";
         std::ofstream output (filename.str().c_str());
         data_out.write_vtk (output);
     }
+    
     template <int dim>
     void
     StokesProblem<dim>::refine_mesh ()
@@ -295,44 +443,43 @@ namespace Step22
                                                          0.3, 0.0);
         triangulation.execute_coarsening_and_refinement ();
     }
+    
     template <int dim>
     void StokesProblem<dim>::run ()
     {
-        {
-            std::vector<unsigned int> subdivisions (dim, 1);
-            subdivisions[0] = 4;
-            const Point<dim> bottom_left = (dim == 2 ?
-                                            Point<dim>(-2,-1) :
-                                            Point<dim>(-2,0,-1));
-            const Point<dim> top_right   = (dim == 2 ?
-                                            Point<dim>(2,0) :
-                                            Point<dim>(2,1,0));
-            GridGenerator::subdivided_hyper_rectangle (triangulation,
-                                                       subdivisions,
-                                                       bottom_left,
-                                                       top_right);
-        }
+        std::vector<unsigned int> subdivisions (dim, 1);
+        subdivisions[0] = 4;
+        
+        const Point<dim> bottom_left = (dim == 2 ?
+                                        Point<dim>(data::left,data::bottom) :
+                                        Point<dim>(-2,0,-1));
+        const Point<dim> top_right   = (dim == 2 ?
+                                        Point<dim>(data::right,data::top) :
+                                        Point<dim>(0,1,0));
+        
+        GridGenerator::subdivided_hyper_rectangle (triangulation,
+                                                   subdivisions, bottom_left, top_right);
+        
         for (typename Triangulation<dim>::active_cell_iterator
              cell = triangulation.begin_active();
              cell != triangulation.end(); ++cell)
             for (unsigned int f=0; f<GeometryInfo<dim>::faces_per_cell; ++f)
-                if (cell->face(f)->center()[dim-1] == 0)
+                if (cell->face(f)->center()[dim-1] == data::top)
                     cell->face(f)->set_all_boundary_ids(1);
-        triangulation.refine_global (4-dim);
-        for (unsigned int refinement_cycle = 0; refinement_cycle<6;
-             ++refinement_cycle)
-        {
-            std::cout << "Refinement cycle " << refinement_cycle << std::endl;
-            if (refinement_cycle > 0)
-                refine_mesh ();
+                else if (cell->face(f)->center()[dim-1] == data::bottom)
+                    cell->face(f)->set_all_boundary_ids(2);
+        
+           triangulation.refine_global (data::refinement_level);
+
+        
+   
             setup_dofs ();
             std::cout << "   Assembling..." << std::endl << std::flush;
             assemble_system ();
             std::cout << "   Solving..." << std::flush;
             solve ();
-            output_results (refinement_cycle);
-            std::cout << std::endl;
-        }
+            output_results ();
+
     }
 }
 int main ()
