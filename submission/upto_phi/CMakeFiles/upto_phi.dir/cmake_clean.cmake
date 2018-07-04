f0394c63bf7689d4a49c8d9c439e39a8678ed9c8
Jane Lee <jane.lee@maths.ox.ac.uk>
2018-07-04 10:38:08 +0100

    dazzler solutions

---
+++ submission/upto_phi/CMakeFiles/upto_phi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/upto_phi.dir/upto_phi.cc.o"
+  "upto_phi.pdb"
+  "upto_phi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/upto_phi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
